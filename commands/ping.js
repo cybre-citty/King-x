6bcf3672812a4a4f9d9fa7d9e4b1d2d4ea875a3b
cybre-citty <153436963+cybre-citty@users.noreply.github.com>
2023-12-29 16:59:45 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -23,6 +23,6 @@ Secktor.cmd({
         const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: 'ඔච්චර තමයි','*🎗️Pong🎗️*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '*🎗️Pong🎗️*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );
