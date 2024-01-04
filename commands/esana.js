13538a821a3c5df7ed4743b293f0933f6d2a68e6
cybre-citty <153436963+cybre-citty@users.noreply.github.com>
2024-01-04 19:46:52 +0530

    Update esana.js

--- commands/esana.js
+++ commands/esana.js
@@ -18,7 +18,7 @@ cmd({
             const ress = await api.news(nn);
             const res = ress.results;
 
-            const txt2 = await Void.sendMessage(citel.chat, {image: {url: res.COVER}, caption: `\n*🚚.. 𝗘𝗦𝗔𝗡𝗔 𝗡𝗘𝗪𝗦 📰 * \n\n*📝* * ᴛɪᴛᴇʟ :* ${res.TITLE}\n\n*🗒️* * ᴅᴀᴛᴇ :* ${res.PUBLISHED}\n\n*🖇️* *⇨ ᴜʀʟ :* ${res.URL}\n\n*📌* * Description :* ${res.DESCRIPTION}\n\n**\n\n𝗞𝗜𝗡𝗚-𝗫 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧`}, { quoted: citel });
+            const txt2 = await Void.sendMessage(citel.chat, {image: {url: res.COVER}, caption: `\n🚚.. 𝗘𝗦𝗔𝗡𝗔 𝗡𝗘𝗪𝗦 📰  \n\n*📝*  𝗧𝗶𝘁𝗲𝗹 : ${res.TITLE}\n\n*🗒️*  𝗗𝗮𝘁𝗲 : ${res.PUBLISHED}\n\n*🖇️*  𝗨𝗿𝗹 : ${res.URL}\n\n*📌*  𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻 : ${res.DESCRIPTION}\n\n𝗞𝗜𝗡𝗚-𝗫 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧`}, { quoted: citel });
 
                 await Void.sendMessage(citel.chat, { react: {
         text: "📰",
