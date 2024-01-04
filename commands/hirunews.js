db2067776e50f418abdf2693bcdd59f5ad072e3b
cybre-citty <153436963+cybre-citty@users.noreply.github.com>
2024-01-04 19:06:21 +0530

    Update hirunews.js

--- commands/hirunews.js
+++ commands/hirunews.js
@@ -13,7 +13,7 @@ cmd({
     const caption = `
    
        🚛..𝐇𝐈𝐑𝐔𝐔 𝐍𝐄𝐖𝐒 🗞️
-     ───────────────────
+  ──────────────────────
     
 📝 *ᴛɪᴛᴇʟ* :  ${hirunew.result.title}
 
@@ -21,7 +21,10 @@ cmd({
 
 🖇️ *ᴜʀʟ* : ${hirunew.result.link}
 
-📌 *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ* : ${hirunew.result.description}`
+📌 *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ* : ${hirunew.result.description}
+
+*𝙺𝙸𝙽𝙶-𝚇 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*
+ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ${Config.ownername}`
 
 await Void.sendMessage(citel.chat, { image: { url: hirunew.result.image }, caption: caption }, { quoted: citel })
 
