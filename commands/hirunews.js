23f9b8d405dc116bf17cc34406a4dbf0e42d03fa
cybre-citty <153436963+cybre-citty@users.noreply.github.com>
2024-01-04 17:24:41 +0530

    Update hirunews.js

--- commands/hirunews.js
+++ commands/hirunews.js
@@ -3,21 +3,27 @@ const hirunews = require('hirunews-scraper')
 cmd({
         pattern: "hirunews",
         category: "hiru",
-        react: "📑",
+        react: "🗞️",
+        alias :['hiru'],
 
         filename: __filename
     },
     async(Void, citel,text) => {
     const hirunew = await hirunews()
-    const caption = ` 𝐇𝐈𝐑𝐔𝐔 𝐍𝐄𝐖𝐒
+    const caption = ` 
+ ╭─────────────────╮
+ │ 🗞️ 𝐇𝐈𝐑𝐔𝐔 𝐍𝐄𝐖𝐒 🚚  │
+ ╰─────────────────╯
+ 📝*ᴛɪᴛᴇʟ* :  ${hirunew.result.title}
 
- *ᴛɪᴛᴇʟ* :  ${hirunew.result.title}
+ ⏳*ᴅᴀᴛᴇ & ᴛɪᴍᴇ* :  ${hirunew.result.dateandtime}
 
- *ᴅᴀᴛᴇ & ᴛɪᴍᴇ* :  ${hirunew.result.dateandtime}
+ 🖇️*ᴜʀʟ* : ${hirunew.result.link}
 
- *ᴜʀʟ* : ${hirunew.result.link}
-
- *ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ* : ${hirunew.result.description} `
+ 📌*ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ* : ${hirunew.result.description} 
+ 
+*𝙺𝙸𝙽𝙶-𝚇 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*
+ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ${Config.ownername}`
 
 await Void.sendMessage(citel.chat, { image: { url: hirunew.result.image }, caption: caption }, { quoted: citel })
 
