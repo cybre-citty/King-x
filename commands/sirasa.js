91eeb4cb8c5a80a687c44c06cc71b6d091804465
cybre-citty <153436963+cybre-citty@users.noreply.github.com>
2024-01-13 20:36:36 +0530

    Update sirasa.js

--- commands/sirasa.js
+++ commands/sirasa.js
@@ -2,7 +2,6 @@ const {cmd } = require('../lib')
 const sirasanews = require('sirasa-news')
 cmd({
         pattern: "sirasa",
-        alias :['සිරස'],
         category: "news",
         react: "📰",
 
@@ -10,18 +9,14 @@ cmd({
     },
     async(Void, citel,text) => {
     const sirasa = await sirasanews()
-    const caption = `🚚.. 𝚂𝙸𝚁𝙰𝚂𝙰 𝙽𝙴𝚆𝚂 📑  
+    const caption = `⛶ _𝐐𝐔𝐄𝐄𝐍 𝐍𝐈𝐂𝐊𝐘  𝐒𝐈𝐑𝐀𝐒𝐀 𝐍𝐄𝐖𝐒_ ⛶    
+🌹⃝⃘̉̉̉̉̉̉🧚 *𝕋𝕀𝕋𝕃𝔼:* ${sirasa.result.title}
 
-   
-📝𝗧𝗜𝗧𝗟𝗘: ${sirasa.result.title}
+🌹⃝⃘̉̉̉̉̉̉🧚 *𝔻𝔸𝕋𝔼 𝔸ℕ𝔻 𝕋𝕀𝕄𝔼* : ${sirasa.result.dateandtime}
 
- ⏳𝗗𝗔𝗧𝗘 𝗔𝗡𝗗 𝗧𝗜𝗠𝗘 : ${sirasa.result.dateandtime}
+🌹⃝⃘̉̉̉̉̉̉🧚 *𝔻𝔼𝕊ℂℝ𝕀𝕃𝕋𝕀𝕆ℕ:* ${sirasa.result.description}
 
- 📌𝗗𝗘𝗦𝗖𝗥𝗟𝗟𝗧𝗜𝗢𝗡: ${sirasa.result.description}
-
- 🖇️𝗡𝗘𝗪𝗦 𝗟𝗜𝗡𝗞: ${sirasa.result.link}
- 
- *𝙺𝙸𝙽𝙶-𝚇 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*`
+🌹⃝⃘̉̉̉̉̉̉🧚 *ℕ𝔼𝕎𝕊 𝕃𝕀ℕ𝕂:* ${sirasa.result.link}`
 
 await Void.sendMessage(citel.chat, { image: { url: sirasa.result.image }, caption: caption }, { quoted: citel })
 
